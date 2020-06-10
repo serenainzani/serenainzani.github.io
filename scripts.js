3a60523fbf8607f86b66105b9625cbc5f4ff5833
Serena <61646611+serenainzani@users.noreply.github.com>
2020-06-10 21:54:57 +0100

    More Info section on index

--- scripts.js
+++ scripts.js
@@ -14,13 +14,19 @@ $(document).ready(function() {
             } else {
                 $('.aboutMe').removeClass('aboutMeChange');
             }
-                        // checks if window is scrolled more than 385px, adds/removes solid class
-                        if($(this).scrollTop() > 400) { 
-                            $('.travel').addClass('travelChange');
-                        } else {
-                            $('.travel').removeClass('travelChange');
-                        }
+            if($(this).scrollTop() > 400) { 
+                $('.travel').addClass('travelChange');
+            } else {
+                $('.travel').removeClass('travelChange');
+            }
+            if($(this).scrollTop() > 730) { 
+                $('.moreInfo').addClass('moreInfoChange');
+            } else {
+                $('.moreInfo').removeClass('moreInfoChange');
+            }
+
         }
+        
         else if (document.title == "Photo Gallery") {
             var numOfElement = document.querySelectorAll( "img" ).length;
                 for (i=1; i < numOfElement; i++){
